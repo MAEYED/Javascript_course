99bf8384644e0df950987d94848a6de2fd0167f3
MAEYED <firstkung44123gg@gmail.com>
2023-03-23 15:15:36 +0700

    nah

---
+++ js/playground.js
@@ -0,0 +1,44 @@
+var temp ="Hello"
+temp = "google"
+
+let temp2 = 100
+temp2 = 50
+
+const temp3 = true
+//temp3 = false
+
+var temp4
+
+const a = 10
+const b = 5
+let c = 5
+c = "uAVE"
+const sum = a+b
+
+console.log(sum)
+console.log(a + b +c);
+
+const temp5 = [1, 2, 3, 4, 5,]
+console.log(temp5[0]);
+
+temp5.forEach(item => {
+    console.log(item)
+})
+
+const filterTemp = temp5.filter((item) => item, >= 3)
+console.log("filter number more than 3 is")
+
+temp5.push(6)
+console.log("insert 6 to array", temp5);
+
+const temp7 = {color: 'red', brand: 'honda', model: 'accord'}
+car1.color = "blue"
+console.log(car1['color'])
+
+/*function sayHello(number, name) => {
+    console.log(`Number is: ${number} by ${name}`);
+}*/
+
+const nickname = "golf"
+
+sayHello(100, nickname)
---
+++ script.js
@@ -0,0 +1,3 @@
+const a = document.querySelector('a')
+console.log(a);
+a.setAttribute('href', 'https://www.google.co.th')
